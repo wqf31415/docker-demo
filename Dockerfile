0777319b20c720284c8be657dcd78c9854d47879
wqf31415 <wqf31415@hotmail.com>
2021-01-13 15:38:56 +0800

    添加使用 dockerfile-maven 插件的配置；

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:8-jre-alpine
+
+ARG JAR_FILE
+ADD target/${JAR_FILE} app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/xyz/wqf/dockerdemo/DockerDemoApplication.java
+++ src/main/java/xyz/wqf/dockerdemo/DockerDemoApplication.java
@@ -4,28 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoApplication {
     private final static Logger log = LoggerFactory.getLogger(DockerDemoApplication.class);
 
     public static void main(String[] args) throws UnknownHostException {
         SpringApplication app = new SpringApplication(DockerDemoApplication.class);
         Environment env = app.run(args).getEnvironment();
-        log.info("\n--+--+--+--+--+--+--+--+--+--+--+--+--+--+--\n" +
-                "Application {} is running! Access URLs:\n" +
+        log.info("\n--+---+---+---+---+---+---+---+---+---+---+--\n" +
+                "Application is running! Access URLs:\n" +
                 "\tLocal:\thttp://localhost:{}\n" +
                 "\tExternal:\thttp://{}:{}\n" +
-                "--+--+--+--+--+--+--+--+--+--+--+--+--+--+--",
-                env.getProperty("spring.application.name"),
+                "--+---+---+---+---+---+---+---+---+---+---+--",
                 env.getProperty("server.port"),
                 InetAddress.getLocalHost().getHostAddress(),
                 env.getProperty("server.port"));
     }
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "world";
+    }
 }
